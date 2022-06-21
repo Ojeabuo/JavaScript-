f5004e5d546e765a1eeacfa8233c2dd4f4a9194a
Samson Afokhai <98502436+Ojeabuo@users.noreply.github.com>
2022-06-21 20:56:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var FName = 'Samson'
+var MName = 'Ojeabuo'
+var LName = 'Afokhai'
+var height = '1.803 metres'
+var country = 'Nigeria'
+
+name = FName + ' ' + MName + ' ' + LName;
+
+document.write('Name:' + ' ' + name +  '<br>')
+document.write('Height:' + ' ' + height + '<br>')
+document.write('Country:' + ' ' + country);
